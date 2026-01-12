cf3da67711713afc2ae8b4bc335359d5a4103210
Prince Gawai <vaibhavgawai2011@gmail.com>
2026-01-12 18:22:34 +0530

    Initial Commit

---
+++ DailyQuoteApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DailyQuoteApp
+//
+//  Created by Prince Gawai on 12/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DailyQuoteApp/DailyQuoteAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailyQuoteAppApp.swift
+//  DailyQuoteApp
+//
+//  Created by Prince Gawai on 12/01/26.
+//
+
+import SwiftUI
+
+@main
+struct DailyQuoteAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
